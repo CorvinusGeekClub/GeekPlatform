02a2a112481eb00fffc03ec889464a50136579c1
Marcell Toth <marcell.toth@geekclub.hu>
2017-03-24 12:59:10 +0100

    Added PostTime in Course_News

--- GeekDatabase/Course_News.sql
+++ GeekDatabase/Course_News.sql
@@ -3,6 +3,7 @@
     [Course_Id]      INT            NOT NULL,
     [Title]     NVARCHAR(MAX)     NOT NULL,
     [Content]   NVARCHAR (MAX) NOT NULL,
+    [PostTime] DATETIME2 NOT NULL, 
     PRIMARY KEY CLUSTERED ([Course_News_Id] ASC),
     CONSTRAINT [FK_Table_ToCourse] FOREIGN KEY ([Course_Id]) REFERENCES [dbo].[Course] ([Course_Id])
 );
