a68d71f01a979ffbc254644adbf885bb01cb558f
Marcell Toth <marcell.toth@geekclub.hu>
2017-03-24 13:09:34 +0100

    Updated Homework_Upload.sql as per discussion in pull request #3

--- GeekDatabase/Homework_Upload.sql
+++ GeekDatabase/Homework_Upload.sql
@@ -1,11 +1,12 @@
 ﻿CREATE TABLE [dbo].[Homework_Upload]
 (
 	[Homework_Upload_Id]	INT NOT NULL PRIMARY KEY IDENTITY,
-	[Course_Thematics_Id]	INT NOT NULL,
+	[Course_Id]	INT NOT NULL,
+	[Week_Number] TINYINT NOT NULL,
 	[Profile_Id]	INT NOT NULL,
 	[Upload_URL]	NVARCHAR(MAX) NOT NULL,
 	[Upload_Date]	DATE NOT NULL,
 	[Comment]	NVARCHAR(MAX) NOT NULL, 
-    CONSTRAINT [FK_Homework_Upload_ToCourse_Thematics] FOREIGN KEY ([Course_Thematics_Id]) REFERENCES [Course_Thematics]([Course_Thematics_Id]),
+    CONSTRAINT [FK_Homework_Upload_ToCourse_Thematics] FOREIGN KEY ([Course_Id], [Week_Number]) REFERENCES [Course_Thematics]([Course_Id],[Week_Number]),
 	CONSTRAINT [FK_Homework_Upload_ToProfile] FOREIGN KEY ([Profile_Id]) REFERENCES [Profile]([Profile_Id])
 );
