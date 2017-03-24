16bc8dac32471cc47eee5098f6b69b9fd5713747
Marcell Toth <marcell.toth@geekclub.hu>
2017-03-24 12:49:26 +0100

    Updated Course.sql as per discussion in pull request #3

--- GeekDatabase/Course.sql
+++ GeekDatabase/Course.sql
@@ -3,9 +3,10 @@
     [Course_Name]       NVARCHAR (MAX) NOT NULL,
     [Description_Short] NVARCHAR (200) NOT NULL,
     [Description_Long]  NVARCHAR (MAX) NOT NULL,
-    [Icon_Large_URL]    NVARCHAR (MAX) NOT NULL,
-    [Icon_Medium_URL]   NVARCHAR (MAX) NOT NULL,
-    [Icon_Small_Url]    NVARCHAR (MAX) NOT NULL,
+    [Icon_FileName]    NVARCHAR (MAX) NOT NULL,
     [Is_Workshop]       BIT            NOT NULL,
+    [Is_Active] BIT NOT NULL , 
+    [Is_Running] BIT NOT NULL, 
+    [SignUp_Deadline] DATE NOT NULL, 
     CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED ([Course_Id] ASC)
 );
