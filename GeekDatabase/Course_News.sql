2571f6a5d5d7261157349e7d0b857c90c5d51574
Marcell Toth <marcell.toth@geekclub.hu>
2017-03-24 12:56:06 +0100

    Updated Course_News.sql as per discussion in pull request #3

--- GeekDatabase/Course_News.sql
+++ GeekDatabase/Course_News.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Course_News] (
     [Course_News_Id] INT            NOT NULL IDENTITY,
     [Course_Id]      INT            NOT NULL,
-    [News_Title]     NCHAR (40)     NOT NULL,
+    [News_Title]     NVARCHAR(MAX)     NOT NULL,
     [News_Content]   NVARCHAR (MAX) NOT NULL,
     PRIMARY KEY CLUSTERED ([Course_News_Id] ASC),
     CONSTRAINT [FK_Table_ToCourse] FOREIGN KEY ([Course_Id]) REFERENCES [dbo].[Course] ([Course_Id])
