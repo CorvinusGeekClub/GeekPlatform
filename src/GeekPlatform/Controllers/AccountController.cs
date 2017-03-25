a4ac7399393a086498174ff0d5df57d95b75fdd2
Marcell Toth <marcell.toth@geekclub.hu>
2017-03-25 20:02:30 +0100

    Added login action

--- src/GeekPlatform/Controllers/AccountController.cs
+++ src/GeekPlatform/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using GeekPlatform.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,36 @@ namespace GeekPlatform.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Login()
+        private readonly UserManager<Profile> _userManager;
+        private readonly SignInManager<Profile> _signInManager;
+
+        public AccountController(UserManager<Profile> userManager, SignInManager<Profile> signInManager)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            if (String.IsNullOrEmpty(email))
+            {
+                return View();
+            }
+
+            Profile user = await _userManager.FindByEmailAsync(email);
+            if (!user.IsActive)
+            {
+                return View();
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+            if (!signInResult.Succeeded)
+            {
+                return View();
+            }
+
+            return Redirect("/");
         }
     }
 }
