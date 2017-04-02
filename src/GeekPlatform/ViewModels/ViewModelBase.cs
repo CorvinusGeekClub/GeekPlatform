5bf2d53e973d5be82fe08ddec35ea278572738ea
Marcell Toth <marcell.toth@geekclub.hu>
2017-04-02 19:28:51 +0200

    Added simple ViewModelBase

---
+++ src/GeekPlatform/ViewModels/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeekPlatform.Models;
+
+namespace GeekPlatform.ViewModels
+{
+    public class ViewModelBase
+    {
+        public String FelhasznaloNev { get; }
+
+        public ViewModelBase(Profile user)
+        {
+            FelhasznaloNev = user.Name;
+        }
+    }
+}
